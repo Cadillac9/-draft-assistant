9ff3fc84cc1da53644b809b3f3d1333f94480755
Cadillac9 <mjyoung3000@hotmail.com>
2026-09-05 16:26:20 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='draft-assistant-v7-scoring-20260905';
+const CACHE='draft-assistant-v7-1-hotfix-20260905';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
